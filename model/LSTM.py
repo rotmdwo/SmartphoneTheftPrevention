224c76a7fe375677d8aa61a3d7ed188c91f7190a
rotmdwo <49435944+rotmdwo@users.noreply.github.com>
2020-07-05 02:41:07 +0900

    파이어베이스로부터 타인들의 데이터 가져오게 모델 수정

--- model/LSTM.py
+++ model/LSTM.py
@@ -113,35 +113,38 @@ def load_data(user_id, seq_len, pred_len):
 
         result.append(secondDataList2D)
 
-    '''
-    # 한 줄 씩 읽어오기
-   
-    f = open(filename, 'r').read()
-    data_string = f.split('\n')
-    d = list() # 9개의 센서 데이터 셋의 나열 => 2차원 배열
+    # 타인들의 데이터 불러오기
+    for user in snapshot_others:
+        if user == user_id:
+            continue
 
-    # 줄 마다 숫자들로 파싱
-    for i in data_string:
-        numbers = i.split(' ')
-        d.append(numbers)
-        #for j in number:
-            #d.append(float(j))
+        snapshot = snapshot_others[user]
 
-    # seq_len 만큼의 데이터 마다 합침 --> 1초 간의 데이터들을 하나로
-    result = []  # 9개의 센서 데이터를 1초 단위로 묶은 것 => 3차원 배열
-    for i in range(int(len(d) / seq_len)):
-        result.append(d[i * seq_len : (i + 1) * seq_len])
-    '''
+        for sec in snapshot:
+            secondDataList2D = []
+            secondDataDictionary2D = snapshot[sec]
+
+            for dataSet in secondDataDictionary2D:
+                oneOver64HzDataList1D = []
+                oneOver64HzDataDictionary1D = secondDataDictionary2D[dataSet]
+
+                for data in oneOver64HzDataDictionary1D:
+                    oneOver64HzDataList1D.append(oneOver64HzDataDictionary1D[data])
+
+                oneOver64HzDataList1D.append(1)  # 스마트폰의 주인이라는 의미하는 class 추가
+                secondDataList2D.append(oneOver64HzDataList1D)
+
+            result.append(secondDataList2D)
+
     # 데이터를 8:2 비율로 Train:Validation 데이터로 나눔
     # 유저의 데이터의 개수가 달라지면 이 부분에서 오류가 남. 리스트의 차원이 정의되지 않음.
     # 그러므로 안드로이드에서 데이터 수집할 때 정확히 5분 동안 같은 양의 데이터가 전송되게 해야 함!
     result = np.array(result)
+    np.random.shuffle(result)
+
     row = int(round(0.8 * result.shape[0]))
     train = result[ : row, : , :]
     test = result[row : , : , :]
-
-    # TODO: 셔플을 Train 데이터와 Test 데이터로 나누기 전에 해야 함. 셔플 전에 하면 Train 데이터에 스마트폰 주인 데이터가 쏠림.
-    np.random.shuffle(train)
 
     # 인풋데이터 파싱
     X_train = train[ : , : , : -pred_len]
@@ -175,11 +178,6 @@ def load_data(user_id, seq_len, pred_len):
 
 #X_train, y_train, X_test, y_test = load_data('sample.txt', seq_len, pred_len)
 X_train, y_train, X_test, y_test = load_data(user_id, seq_len, pred_len)
-'''
-ref = db.reference('1').child('Accelerometer').child('1')
-row = ref.get()
-print(row)
-
 
 # Input은 3차원 , Output은 2차원 데이터를 필요로 함
 # input_shape에서 맨 앞 차원(데이터 수)는 적지 않고 2차원, 3차원 크기만 씀
@@ -204,4 +202,3 @@ model.fit(np.array(X_train), np.array(y_train), batch_size= 32, epochs=epochs, v
 
 print(model.output.op.name)
 print(model.input.op.name)
-'''
